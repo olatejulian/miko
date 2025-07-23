841f312b80af740c1bd8332195ead507f21c941b
olatejulian <julian.lf.olate@gmail.com>
2025-07-23 10:35:10 -0300

    refactor: :building_construction: add init file for better importing

---
+++ miko/core/components/__init__.py
@@ -0,0 +1,25 @@
+from .animation import Animation
+from .collider import Collider
+from .direction import Direction
+from .input import Input
+from .map_position import MapPosition
+from .npc import NPC
+from .player import Player
+from .position import Position
+from .script_trigger import ScriptTrigger
+from .sprite import Sprite
+from .velocity import Velocity
+
+__all__ = [
+    "Animation",
+    "Collider",
+    "Direction",
+    "Input",
+    "MapPosition",
+    "NPC",
+    "Player",
+    "Position",
+    "ScriptTrigger",
+    "Sprite",
+    "Velocity",
+]
